a24de49bdd07b752b2a109cfe0433290c0854526
Stephen Fortune <steve.fortune@icecb.com>
2017-10-21 19:01:56 +0100

    Deleted old Todo cmds

--- Input and Output/Todo/ReadTodos.hs
+++
@@ -1,9 +0,0 @@
-import System.IO
-import Todo
-
-main = do
-  putStrLn welcomeMessage
-  putTodos
-    where welcomeMessage = "Welcome to your todo list. The following " ++
-            "are your todos:\n"
-
